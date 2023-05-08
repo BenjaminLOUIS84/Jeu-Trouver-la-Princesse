264de1258e0d38aad17717c4f20fd4ae55cc688e
Benjamin Louis <louisbenetval@orange.fr>
2023-05-08 03:54:08 +0200

    Jeu trouver la Princesse

---
+++ jeu/script.js
@@ -0,0 +1,127 @@
+//  ETAPE   4   Créer les fonctions
+
+//Pour rendre l'ordre des portes aléatoire
+
+function shuffleChildren(parent) {
+    let children = parent.children
+    let i = children.length, k, temp
+    while (--i > 0) {
+
+        k = Math.floor(Math.random() * (i + 1))
+        temp = children[k]
+        children[k] = children[i]
+        parent.appendChild(temp)
+    }
+}
+
+//  ETAPE   1.1   Créer les 3 Vies
+
+const box = document.createElement("div")
+const box0 = document.createElement("div")
+const box1 = document.createElement("div")
+
+box.classList.add("heart")
+box0.classList.add("heart")
+box1.classList.add("heart")
+
+const board1 = document.querySelector("#life")
+board1.appendChild(box)
+board1.appendChild(box0)
+board1.appendChild(box1)
+
+//  ETAPE   1.2   Créer les Succès (apparait chaque fois que la princesse est trouvée)
+
+const box2 = document.createElement("div")
+const box3 = document.createElement("div")
+const box4 = document.createElement("div")
+
+box2.classList.add("star")
+box3.classList.add("star")
+box4.classList.add("star")
+
+const board2 = document.querySelector("#success")
+
+//  ETAPE   2   Créer les 3 Portes
+
+const box5 = document.createElement("div")
+
+box5.classList.add("door-close")
+
+const board5 = document.querySelector("#door")
+
+let nb = 1
+
+for (let i = 1; i <= 3; i++) {
+    const newbox5 = box5.cloneNode()
+    board5.appendChild(newbox5)
+
+    newbox5.addEventListener("click", function () {
+        console.log("Porte n°" + i + ", click!")
+
+        //Pour ouvrir les portes dans n'importe quel ordre
+        //et tomber au hasard sur une porte vide, la princesse ou ganon
+
+        if (i => nb) {
+            newbox5.classList.add("door-open")
+            if (i == 1) {
+                newbox5.classList.add("door-open")
+            }
+            else if (i == 2) {
+                newbox5.classList.add("princesse")
+            }
+            else if (i == 3) {
+                newbox5.classList.add("ganon")
+            }
+            nb++
+        }
+
+        //Une vie en moins lorsque ganon est dernière la porte
+
+        if (i == 3) {
+
+            board5.querySelectorAll(".door-close").forEach(function (validBox) {
+                validBox.classList.remove("door-close")
+            })
+
+            board1.removeChild(box)
+        }
+
+        //Un succès en plus lorsque princesse est dernière la porte
+
+        if (i == 2) {
+
+            board5.querySelectorAll(".door-close").forEach(function (validBox) {
+                validBox.classList.remove("door-close")
+            })
+
+            board2.appendChild(box2)
+
+        }
+
+    })
+
+}
+
+//  ETAPE   3   Créer un bouton Reset
+
+const box6 = document.createElement("div")
+
+box6.classList.add("button")
+
+const board3 = document.querySelector("#reset")
+
+board3.appendChild(box6)
+
+//box6.innerText =/////////////////////////////////////////////Comment mettre du texte?
+
+box6.addEventListener("click", function () {
+    console.log("Bouton" + "click!")
+
+    board5.removeChild(box5)
+
+})
+
+//  ETAPE   5   Matérialiser les fonctions
+
+shuffleChildren(board5)
+
